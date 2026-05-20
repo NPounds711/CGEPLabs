de45b429c5d619a5f90968c641cfebea449d2d7b
npounds0802-netizen <npounds0802@gmail.com>
2026-05-20 11:44:21 -0400

    lab 5.4: deploy GCP WIF, audit logs, and org policy reference   (AC-2, IA-5, AU-2, AU-12, CM-6)

---
+++ terraform/baselines/gcp/variables.tf
@@ -0,0 +1,11 @@
+variable "gcp_project" {
+  description = "GCP project ID"
+  type        = string
+  default     = "corded-keel-450918-i6"
+}
+
+variable "github_repo" {
+  description = "GitHub repo for WIF attribute condition (owner/repo)"
+  type        = string
+  default     = "NPounds711/CGEPLabs"
+}
